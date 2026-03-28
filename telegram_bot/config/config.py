8952ed5e2e13bb3286519d1deabb74d7e80047ea
Короленко Григорий Дмитриевич <korolenkogrigorijdmitrievic@MacBook-Air-Korolenko.local>
2026-03-28 23:33:49 +0300

    fixed registration

--- telegram_bot/config/config.py
+++ telegram_bot/config/config.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv("Token.env")
+load_dotenv("telegram_bot/config/Token.env")
 
 TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+ADMIN_ID = os.getenv("ADMIN_ID")
